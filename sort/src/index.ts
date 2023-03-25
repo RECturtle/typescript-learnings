d7494e0a2a856db1ca513e19ad3a93ec29e2934c
RECturtle <spencer.mertes@gmail.com>
2023-03-24 23:03:06 -0400

    feat: move to abstract class and add linked list support

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,18 +1,17 @@
 import { Sorter } from "./Sorter";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedLIst";
 import { NumbersCollection } from "./NumbersCollection";
 
 const characterCollection = new CharactersCollection("zacbIHkj");
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const linkedList = new LinkedList();
 
-const numberSorter = new Sorter(numbersCollection);
-const characterSorter = new Sorter(characterCollection);
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
 
-
-numbersCollection.printData();
-numberSorter.sort();
-numbersCollection.printData();
-
-characterCollection.printData();
-characterSorter.sort();
-characterCollection.printData();
+characterCollection.sort();
+numbersCollection.sort();
+linkedList.sort();
