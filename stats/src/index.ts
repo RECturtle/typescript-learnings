d3191e82e69580e240ce257419b485625f8c332b
RECturtle <spencer.mertes@gmail.com>
2023-03-25 21:37:13 -0400

    feat: start building our file reader

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello, world!');
+import fs from 'fs';
+
+const matches = fs
+	.readFileSync('football.csv', {
+		encoding: 'utf-8',
+	})
+	.split('\n')
+	.map((row: string) => row.split(','));
+
+let manWon = 0;
+
+for (let match of matches) {
+	if (
+		(match[1] === 'Man United' && match[5] === 'H') ||
+		(match[2] === 'Man United' && match[5] === 'A')
+	) {
+		manWon++;
+	}
+}
+
+console.log(`Man United won ${manWon} games`);
